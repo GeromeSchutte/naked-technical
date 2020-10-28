e0fcbeb31b066834287f48ecf44dcfe73abe28fa
Gerome <gerome.schutte@hotmail.com>
2020-10-29 00:19:07 +0200

    Move basket into page

---
+++ src/Pages/BasketPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BasketPageBody from '../Components/BasketPageBody/BasketPageBody';
+import Layout from '../Components/Layout/Layout';
+
+const BasketPage = () => {
+    return <Layout>
+        <BasketPageBody/>
+    </Layout>;
+};
+
+export default BasketPage;
